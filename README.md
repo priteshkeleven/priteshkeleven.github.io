714d6a5f106379a881205c83beee7d1c02df57c1
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-01-18 14:19:29 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pritesh K - portfolio
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <>
+            <header className={"header"}>
+                <nav>
+                    <h3>Pritesh K.</h3>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default Header;
