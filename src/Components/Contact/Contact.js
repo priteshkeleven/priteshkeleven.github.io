cdfdda76b4e876ff38c4a06fa994f3e3974eae70
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-01-21 21:23:49 +0530

    Added Contact Component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Contact.scss";
+import contactMe from "./../../Assets/image/contact-me.svg";
+
+const Contact = () => {
+    return (
+        <>
+            <div className={"contact"}>
+                <div className={"contact-form"}>
+                    <div>
+                        <input type={"text"} className={"input name"} placeholder={"Your Name, Please!"}/>
+                    </div>
+                    <div>
+                        <input type={"email"} className={"input email"} placeholder={"Email Address Too, Thanks!!"}/>
+                    </div>
+                    <div>
+                        <textarea rows={5} className={"input message"} placeholder={"Greetings!!! Your Message Goes Here!"}/>
+                    </div>
+                    <div>
+                        <button className={"send-button"}>Send 📡</button>
+                    </div>
+                </div>
+                <div className={"contact-img-container"}>
+                    <img src={contactMe} className={"contact-img"} alt={"contact-form"}/>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Contact;
--- src/Layout/Body/Body.js
+++ src/Layout/Body/Body.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Body.scss";
 import About from "../../Components/About/About";
+import Contact from "../../Components/Contact/Contact";
 
 const Body = () => {
     return (
         <>
             <About/>
+            <Contact/>
         </>
     );
 };
