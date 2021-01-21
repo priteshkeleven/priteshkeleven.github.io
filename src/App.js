71e2aac48c2d5cb054f382ca9f52d58cb8be09fc
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-01-21 17:50:20 +0530

    Added header menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
-import Header from "./Components/Header/Header";
+import Header from "./Layout/Header/Header";
+import Body from "./Layout/Body/Body";
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Body/>
     </>
   );
 }
---
+++ src/Components/HeaderMenu/HeaderMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HeaderMenu.scss";
+
+const HeaderMenu = () => {
+    return (
+        <>
+            <ul className={"header-menu"}>
+                <li><a href={"#about"}>About</a></li>
+                <li><a href={"#contact"}>Contact</a></li>
+            </ul>
+        </>
+    );
+};
+
+export default HeaderMenu;
