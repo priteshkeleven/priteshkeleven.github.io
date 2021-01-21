ca8c4ebba941303c8a14a66385c84f71bc65b071
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-01-21 17:50:58 +0530

    Changed File Structure

---
+++ src/Layout/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.scss";
+import HeaderMenu from "../../Components/HeaderMenu/HeaderMenu";
+
+const Header = () => {
+    return (
+        <>
+            <header className={"header"}>
+                <nav className={"nav"}>
+                    <h3 className={"heading"}>
+                        <a href={"https://priteshkeleven.github.io"}>Pritesh K.</a>
+                    </h3>
+                </nav>
+            </header>
+            <HeaderMenu/>
+        </>
+    );
+};
+
+export default Header;
