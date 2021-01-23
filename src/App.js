e675c573aeeb0a4db8ddd9a527601f096aa7a990
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-01-23 13:19:22 +0530

    Integrated Contact form with Firebase

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@ import './App.scss';
 import Header from "./Layout/Header/Header";
 import Body from "./Layout/Body/Body";
 import Footer from "./Layout/Footer/Footer";
+import firebase from "firebase";
 
 const App = () => {
-  return (
-    <>
-      <Header/>
-      <Body/>
-      <Footer/>
-    </>
-  );
+    const {
+        REACT_APP_API_KEY,
+        REACT_APP_AUTH_DOMAIN,
+        REACT_APP_PROJECT_ID,
+        REACT_APP_STORAGE_BUCKET,
+        REACT_APP_MESSAGING_SENDER_ID,
+        REACT_APP_APP_ID,
+        REACT_APP_MEASUREMENT_ID
+    } = process.env;
+
+    const firebaseConfig = {
+        apiKey: REACT_APP_API_KEY,
+        authDomain: REACT_APP_AUTH_DOMAIN,
+        projectId: REACT_APP_PROJECT_ID,
+        storageBucket: REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+        appId: REACT_APP_APP_ID,
+        measurementId: REACT_APP_MEASUREMENT_ID,
+    };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app()
+    }
+    firebase.analytics();
+
+    return (
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,30 +1,115 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import "./Contact.scss";
 import contactMe from "./../../Assets/image/contact-me.svg";
+import firebase from "firebase/app";
 
 const Contact = () => {
+    const db = firebase.firestore();
+
+    const [lead, setLead] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+    const [sent, setSent] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [messageError, setMessageError] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        if (name === "name" && nameError) setNameError(false);
+        if (name === "email" && emailError) setEmailError(false);
+        if (name === "message" && setMessageError) setMessageError(false);
+        setLead(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = () => {
+        if (lead.name === "") {
+            setNameError(true);
+        }
+        if (lead.email === "" || !(/\S+@\S+\.\S+/.test(lead.email))) {
+            setEmailError(true);
+        }
+        if (lead.message === "") {
+            setMessageError(true);
+        }
+        if (lead.name !== "" && lead.email !== "" && lead.message !== "" && (/\S+@\S+\.\S+/.test(lead.email))) {
+            setLoading(true);
+            db.collection("leads")
+                .add({
+                    ...lead,
+                    datetime: firebase.firestore.Timestamp.fromDate(new Date())
+                })
+                .then(docRef => {
+                    setSent(true);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    setError(true);
+                    setLoading(false);
+                });
+        }
+    }
+
     return (
         <>
             <div className={"contact"} id={"contact"}>
                 <div className={"contact-form"}>
-                    <div>
-                        <input type={"text"} className={"input name"} placeholder={"Your Name, Please!"}/>
-                    </div>
-                    <div>
-                        <input type={"email"} className={"input email"} placeholder={"Email Address Too, Thanks!!"}/>
-                    </div>
-                    <div>
-                        <textarea rows={5} className={"input message"} placeholder={"Greetings!!! Your Message Goes Here!"}/>
-                    </div>
-                    <div>
-                        <button className={"send-button"}>Send 📡</button>
-                    </div>
+                    {
+                        !sent && !error ? (
+                            <Fragment>
+                                <div className={"form-control"}>
+                                    <input type={"text"} className={"input name"} name={"name"}
+                                           placeholder={"Your Name, Please!"}
+                                           onChange={handleChange} value={lead.name}/>
+                                    {nameError ? <span className={"validation-text"}>Name shouldn't be empty!!</span> : <Fragment/>}
+                                </div>
+                                <div className={"form-control"}>
+                                    <input type={"email"} className={"input email"} name={"email"}
+                                           value={lead.email}
+                                           placeholder={"Email Address Too, Thanks!!"} onChange={handleChange}/>
+                                    {emailError ? <span className={"validation-text"}>Email shouldn't be empty or invalid!!</span> : <Fragment/>}
+
+                                </div>
+                                <div className={"form-control"}>
+                                     <textarea rows={5} className={"input message"} name={"message"}
+                                               value={lead.message}
+                                               placeholder={"Greetings!!! Your Message Goes Here!"}
+                                               onChange={handleChange}/>
+                                    {messageError ? <span className={"validation-text"}>Message looks empty!! It shouldn't.</span> : <Fragment/>}
+                                </div>
+                                <div className={"form-control"}>
+                                    <button className={"send-button"} onClick={handleSubmit}>{loading ? `Loading...` : `Send 📡`}</button>
+                                </div>
+                            </Fragment>
+                        ) : <Fragment/>
+                    }
+                    {
+                        sent ? (
+                            <div>
+                                <button className={"send-button"} disabled>Sent! Will get back to you soon. 👍</button>
+                            </div>
+                        ) : <Fragment/>
+                    }
+                    {
+                        error ? (
+                            <div>
+                                <button className={"send-button"} disabled>Error! Try refreshing page. ⚠️</button>
+                            </div>
+                        ) : <Fragment/>
+                    }
+
                 </div>
                 <div className={"contact-img-container"}>
                     <img src={contactMe} className={"contact-img"} alt={"contact-form"}/>
                 </div>
             </div>
-
         </>
     );
 };
